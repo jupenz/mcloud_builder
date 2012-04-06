3b1dfbc42d8b9f8cc7b3db2e0753c21a6ffee08d
Elizar Pepino <jupenz@gmail.com>
2012-04-06 16:07:45 +0800

    refactored js plugins

--- js/cloud-builder.js
+++ js/cloud-builder.js
@@ -379,18 +379,122 @@ $(function(){
 	
 	};
 
+  // HIT TEST OBJECT PLUGIN
+  jQuery.fn.hitTestObject=function(target){
+  	var src=$(this);
+  	var x1=src.offset().left,y1=src.offset().top,w=src.outerWidth(),h=src.outerHeight();
+  	var xp1=target.offset().left,yp1=target.offset().top,wp=target.outerWidth(),hp=target.outerHeight();
+  	var x2=x1+w,y2=y1+h,xp2=xp1+wp,yp2=yp1+hp;
 
-	// Init App
-	var _computes = new ComputeTypes();
-	var _volumes = new DiskVolumes();
-	var _project = new Project();
-	var _computesView = new ComputeTypesView({collection: _computes});
-	var _volumesView = new DiskVolumesView({collection: _volumes});
-	var _projectView = new ProjectView({collection: _project});
-	var _paper = new Raphael("cb-stage");
-	var _connections = [];
-	var _pendings = [];
-	_computes.fetch();
-	_volumes.fetch();
+  	if(xp1>=x1 && xp1<=x2 )
+  	{
+  		if(yp1>=y1 && yp1<=y2)
+  		{
+  			return true;
+  		}
+  		else if(y1>=yp1 && y1<=yp2)
+  		{
+  			return true;
+  		}
+  	}
+  	else if(x1>=xp1 && x1<=xp2)
+  	{
+  		if(yp1>=y1 && yp1<=y2)
+  		{
+  			return true;
+  		}
+  		else if(y1>=yp1 && y1<=yp2)
+  		{
+  			return true;
+  		}
+  	}
+  	return false;
+  };
+  
+  // CONNECTION PLUGIN
+  Raphael.fn.connection = function (obj1, obj2, line, bg) {
+
+      if (obj1.line && obj1.from && obj1.to) {
+          line = obj1;
+          obj1 = line.from;
+          obj2 = line.to;
+      }
+      var el1 = obj1.$el, el2 = obj2.$el,
+          box1 = {
+            x: el1.position().left,
+            y: el1.position().top,
+            width: el1.width(),
+            height: el1.height()
+          },
+          box2 = {
+            x: el2.position().left,
+            y: el2.position().top,
+            width: el2.width(),
+            height: el2.height()
+          };
+      var bb1 = box1,
+          bb2 = box2,
+          p = [{x: bb1.x + bb1.width / 2, y: bb1.y - 1},
+          {x: bb1.x + bb1.width / 2, y: bb1.y + bb1.height + 1},
+          {x: bb1.x - 1, y: bb1.y + bb1.height / 2},
+          {x: bb1.x + bb1.width + 1, y: bb1.y + bb1.height / 2},
+          {x: bb2.x + bb2.width / 2, y: bb2.y - 1},
+          {x: bb2.x + bb2.width / 2, y: bb2.y + bb2.height + 1},
+          {x: bb2.x - 1, y: bb2.y + bb2.height / 2},
+          {x: bb2.x + bb2.width + 1, y: bb2.y + bb2.height / 2}],
+          d = {}, dis = [];
+      for (var i = 0; i < 4; i++) {
+          for (var j = 4; j < 8; j++) {
+              var dx = Math.abs(p[i].x - p[j].x),
+                  dy = Math.abs(p[i].y - p[j].y);
+              if ((i == j - 4) || (((i != 3 && j != 6) || p[i].x < p[j].x) && ((i != 2 && j != 7) || p[i].x > p[j].x) && ((i != 0 && j != 5) || p[i].y > p[j].y) && ((i != 1 && j != 4) || p[i].y < p[j].y))) {
+                  dis.push(dx + dy);
+                  d[dis[dis.length - 1]] = [i, j];
+              }
+          }
+      }
+      if (dis.length == 0) {
+          var res = [0, 4];
+      } else {
+          res = d[Math.min.apply(Math, dis)];
+      }
+      var x1 = p[res[0]].x,
+          y1 = p[res[0]].y,
+          x4 = p[res[1]].x,
+          y4 = p[res[1]].y;
+      dx = Math.max(Math.abs(x1 - x4) / 2, 10);
+      dy = Math.max(Math.abs(y1 - y4) / 2, 10);
+      var x2 = [x1, x1, x1 - dx, x1 + dx][res[0]].toFixed(3),
+          y2 = [y1 - dy, y1 + dy, y1, y1][res[0]].toFixed(3),
+          x3 = [0, 0, 0, 0, x4, x4, x4 - dx, x4 + dx][res[1]].toFixed(3),
+          y3 = [0, 0, 0, 0, y1 + dy, y1 - dy, y4, y4][res[1]].toFixed(3);
+      var path = ["M", x1.toFixed(3), y1.toFixed(3), "C", x2, y2, x3, y3, x4.toFixed(3), y4.toFixed(3)].join(",");
+      if (line && line.line) {
+          line.bg && line.bg.attr({path: path});
+          line.line.attr({path: path});
+      } else {
+          var color = typeof line == "string" ? line : "#000";
+          return {
+              bg: bg && bg.split && this.path(path).attr({stroke: bg.split("|")[0], fill: "none", "stroke-width": bg.split("|")[1] || 3}),
+              line: this.path(path).attr({stroke: color.split("|")[0] || "#000", fill: "none", "stroke-width": color.split("|")[1] || 1}),
+              from: obj1,
+              to: obj2
+          };
+      }
+  };
+  
+  // Init App
+  var _computes = new ComputeTypes();
+  var _volumes = new DiskVolumes();
+  var _project = new Project();
+  var _computesView = new ComputeTypesView({collection: _computes});
+  var _volumesView = new DiskVolumesView({collection: _volumes});
+  var _projectView = new ProjectView({collection: _project});
+  var _paper = new Raphael("cb-stage");
+  var _connections = [];
+  var _pendings = [];
+  
+  _computes.fetch();
+  _volumes.fetch();
 	
 });
---
+++ js/ui-component.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+
+	// COMBO BOX SETUP
+	$('a.combo-box-link').click(function(e){
+
+		var self = $(this);
+		var parent = self.parent();
+
+		parent.children("ul.combo-box").css({
+			top: parent.outerHeight() + 3,
+			left: 0
+		});
+		
+
+		if (self.hasClass("active") && parent.hasClass("combo-box-active")) {
+			self.removeClass("active");
+			parent.removeClass("combo-box-active");
+		}
+		else {
+
+			// Reset active combo-box
+			var resetTarget = $('li.combo-box-active');
+			resetTarget.children('a.active').removeClass("active");
+			resetTarget.removeClass("combo-box-active");
+			// Activate this one
+			self.addClass("active");
+			parent.addClass("combo-box-active");
+		}
+		
+		e.preventDefault();
+		e.stopPropagation();
+		
+		$(window).bind("click", function(){
+			if(parent.hasClass("combo-box-active")) parent.removeClass("combo-box-active");
+			if (self.hasClass("active")) self.removeClass("active");
+		});
+		
+	
+	});
+	
+
+	var resizeStage = function(){
+		// Stage and SideNav Setup
+		var _stage = $('div#cb-stage');
+		var _sideNav = $('aside.compute-type-section');
+		var offsetHeight = $('header#header').outerHeight(true) + 40;
+		var computedHeight = $(window).height() - offsetHeight;
+		var _stageWidth = $(window).width() - _sideNav.outerWidth(true) - 40;
+		_sideNav.height(computedHeight);
+		_stage.width(_stageWidth).height(computedHeight);
+		_stage.children('svg').height(_stage.height()).width(_stage.width());
+	};
+	
+	resizeStage();
+	$(window).resize(resizeStage);
+
+	// TAB SETUP
+	
+	$("a.save").click(function(e){
+	  // TODO: save project and connections
+	});
+
+});
